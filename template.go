92c52a2ab675e8ad8bd4d88ee9ec6b5eab95a80e
liushuliang <shuliang.liu@capstones.cn>
2023-02-06 16:18:44 +0800

    存一个模板

--- template.go
+++ template.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -10,423 +11,96 @@ import (
 
 var wrtr = bufio.NewWriterSize(os.Stdout, 10_000_000)
 var rdr = bufio.NewScanner(os.Stdin)
-
-func gs() string { rdr.Scan(); return rdr.Text() }
-func gi() int {
-	i, e := strconv.Atoi(gs())
-	if e != nil {
-		panic(e)
-	}
-	return i
-}
-func gis(n int) []int {
-	res := make([]int, n)
-	for i := 0; i < n; i++ {
-		res[i] = gi()
-	}
-	return res
-}
-func gf() float64 {
-	f, e := strconv.ParseFloat(gs(), 64)
-	if e != nil {
-		panic(e)
-	}
-	return f
-}
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
 func gbs() []byte { return []byte(gs()) }
-func gfs(n int) []float64 {
-	res := make([]float64, n)
-	for i := 0; i < n; i++ {
-		res[i] = gf()
-	}
-	return res
+func gfs(n int) []float64  { res := make([]float64,n); for i:=0;i<n;i++ { res[i] = gf() }; return res }
+func gss(n int) []string  { res := make([]string,n); for i:=0;i<n;i++ { res[i] = gs() }; return res }
+func gi64() int64     { i,e := strconv.ParseInt(gs(),10,64); if e != nil {panic(e)}; return i }
+func gis64(n int) []int64  { res := make([]int64,n); for i:=0;i<n;i++ { res[i] = gi64() }; return res }
+
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
 }
-func gss(n int) []string {
-	res := make([]string, n)
-	for i := 0; i < n; i++ {
-		res[i] = gs()
-	}
-	return res
-}
-func gi64() int64 {
-	i, e := strconv.ParseInt(gs(), 10, 64)
-	if e != nil {
-		panic(e)
-	}
-	return i
-}
-func gis64(n int) []int64 {
-	res := make([]int64, n)
-	for i := 0; i < n; i++ {
-		res[i] = gi64()
-	}
-	return res
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
 }
 
-func ia(m int) []int { return make([]int, m) }
-func iai(m int, v int) []int {
-	a := make([]int, m)
-	for i := 0; i < m; i++ {
-		a[i] = v
-	}
-	return a
+func ia64(m int) []int64 { return make([]int64,m) }
+func iai64(m int,v int64) []int64 { a := make([]int64,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi64(n int,m int,v int64) [][]int64 {
+	r := make([][]int64,n); for i:=0;i<n;i++ { x := make([]int64,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
 }
-func twodi(n int, m int, v int) [][]int {
-	r := make([][]int, n)
-	for i := 0; i < n; i++ {
-		x := make([]int, m)
-		for j := 0; j < m; j++ {
-			x[j] = v
-		}
-		r[i] = x
-	}
-	return r
-}
-func fill2(m int) ([]int, []int) {
-	a, b := ia(m), ia(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i] = gi(), gi()
-	}
-	return a, b
-}
-func fill3(m int) ([]int, []int, []int) {
-	a, b, c := ia(m), ia(m), ia(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i], c[i] = gi(), gi(), gi()
-	}
-	return a, b, c
-}
-func fill4(m int) ([]int, []int, []int, []int) {
-	a, b, c, d := ia(m), ia(m), ia(m), ia(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i], c[i], d[i] = gi(), gi(), gi(), gi()
-	}
-	return a, b, c, d
-}
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
-func rev(a []int) {
-	i, j := 0, len(a)-1
-	for i < j {
-		a[i], a[j] = a[j], a[i]
-		i++
-		j--
-	}
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
-}
-func maxarr(a []int) int {
-	ans := a[0]
-	for _, aa := range a {
-		if aa > ans {
-			ans = aa
-		}
-	}
-	return ans
-}
-func minarr(a []int) int {
-	ans := a[0]
-	for _, aa := range a {
-		if aa < ans {
-			ans = aa
-		}
-	}
-	return ans
-}
-func sumarr(a []int) int {
-	ans := 0
-	for _, aa := range a {
-		ans += aa
-	}
-	return ans
-}
-func zeroarr(a []int) {
-	for i := 0; i < len(a); i++ {
-		a[i] = 0
-	}
-}
-func powmod(a, e, mod int) int {
-	res, m := 1, a
-	for e > 0 {
-		if e&1 != 0 {
-			res = res * m % mod
-		}
-		m = m * m % mod
-		e >>= 1
-	}
-	return res
-}
-func powint(a, e int) int {
-	res, m := 1, a
-	for e > 0 {
-		if e&1 != 0 {
-			res = res * m
-		}
-		m = m * m
-		e >>= 1
-	}
-	return res
-}
-func gcd(a, b int) int {
-	for b != 0 {
-		t := b
-		b = a % b
-		a = t
-	}
-	return a
-}
-func gcdExtended(a, b int) (int, int, int) {
-	if a == 0 {
-		return b, 0, 1
-	}
-	gcd, x1, y1 := gcdExtended(b%a, a)
-	return gcd, y1 - (b/a)*x1, x1
-}
-func modinv(a, m int) (int, bool) {
-	g, x, _ := gcdExtended(a, m)
-	if g != 1 {
-		return 0, false
-	}
-	return (x%m + m) % m, true
-}
-func vecintstring(a []int) string {
-	astr := make([]string, len(a))
-	for i, a := range a {
-		astr[i] = strconv.Itoa(a)
-	}
-	return strings.Join(astr, " ")
-}
-func makefact(n int, mod int) ([]int, []int) {
-	fact, factinv := make([]int, n+1), make([]int, n+1)
-	fact[0] = 1
-	for i := 1; i <= n; i++ {
-		fact[i] = fact[i-1] * i % mod
-	}
-	factinv[n] = powmod(fact[n], mod-2, mod)
-	for i := n - 1; i >= 0; i-- {
-		factinv[i] = factinv[i+1] * (i + 1) % mod
-	}
-	return fact, factinv
-}
-
-func ia64(m int) []int64 { return make([]int64, m) }
-func iai64(m int, v int64) []int64 {
-	a := make([]int64, m)
-	for i := 0; i < m; i++ {
-		a[i] = v
-	}
-	return a
-}
-func twodi64(n int, m int, v int64) [][]int64 {
-	r := make([][]int64, n)
-	for i := 0; i < n; i++ {
-		x := make([]int64, m)
-		for j := 0; j < m; j++ {
-			x[j] = v
-		}
-		r[i] = x
-	}
-	return r
-}
-func fill264(m int) ([]int64, []int64) {
-	a, b := ia64(m), ia64(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i] = gi64(), gi64()
-	}
-	return a, b
-}
-func fill364(m int) ([]int64, []int64, []int64) {
-	a, b, c := ia64(m), ia64(m), ia64(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i], c[i] = gi64(), gi64(), gi64()
-	}
-	return a, b, c
-}
-func fill464(m int) ([]int64, []int64, []int64, []int64) {
-	a, b, c, d := ia64(m), ia64(m), ia64(m), ia64(m)
-	for i := 0; i < m; i++ {
-		a[i], b[i], c[i], d[i] = gi64(), gi64(), gi64(), gi64()
-	}
-	return a, b, c, d
-}
-func abs64(a int64) int64 {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
-func rev64(a []int64) {
-	i, j := 0, len(a)-1
-	for i < j {
-		a[i], a[j] = a[j], a[i]
-		i++
-		j--
-	}
-}
-func max64(a, b int64) int64 {
-	if a > b {
-		return a
-	}
-	return b
-}
-func min64(a, b int64) int64 {
-	if a > b {
-		return b
-	}
-	return a
-}
-func maxarr64(a []int64) int64 {
-	ans := a[0]
-	for _, aa := range a {
-		if aa > ans {
-			ans = aa
-		}
-	}
-	return ans
-}
-func minarr64(a []int64) int64 {
-	ans := a[0]
-	for _, aa := range a {
-		if aa < ans {
-			ans = aa
-		}
-	}
-	return ans
-}
-func sumarr64(a []int64) int64 {
-	ans := int64(0)
-	for _, aa := range a {
-		ans += aa
-	}
-	return ans
-}
-func zeroarr64(a []int64) {
-	for i := 0; i < len(a); i++ {
-		a[i] = 0
-	}
-}
-func powmod64(a, e, mod int64) int64 {
-	res, m := int64(1), a
-	for e > 0 {
-		if e&1 != 0 {
-			res = res * m % mod
-		}
-		m = m * m % mod
-		e >>= 1
-	}
-	return res
-}
-func powint64(a, e int64) int64 {
-	res, m := int64(1), a
-	for e > 0 {
-		if e&1 != 0 {
-			res = res * m
-		}
-		m = m * m
-		e >>= 1
-	}
-	return res
-}
-func gcd64(a, b int64) int64 {
-	for b != 0 {
-		t := b
-		b = a % b
-		a = t
-	}
-	return a
-}
-func gcdExtended64(a, b int64) (int64, int64, int64) {
-	if a == 0 {
-		return b, 0, 1
-	}
-	gcd, x1, y1 := gcdExtended64(b%a, a)
-	return gcd, y1 - (b/a)*x1, x1
-}
-func modinv64(a, m int64) (int64, bool) {
-	g, x, _ := gcdExtended64(a, m)
-	if g != 1 {
-		return 0, false
-	}
-	return (x%m + m) % m, true
-}
-func vecint64string(a []int64) string {
-	astr := make([]string, len(a))
-	for i, a := range a {
-		astr[i] = strconv.FormatInt(a, 10)
-	}
-	return strings.Join(astr, " ")
-}
-func makefact64(n int, mod int64) ([]int64, []int64) {
-	fact, factinv := make([]int64, n+1), make([]int64, n+1)
-	fact[0] = 1
-	for i := 1; i <= n; i++ {
-		fact[i] = fact[i-1] * int64(i) % mod
-	}
-	factinv[n] = powmod64(fact[n], mod-2, mod)
-	for i := n - 1; i >= 0; i-- {
-		factinv[i] = factinv[i+1] * int64(i+1) % mod
-	}
-	return fact, factinv
+func fill264(m int) ([]int64,[]int64) { a,b := ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i] = gi64(),gi64()}; return a,b }
+func fill364(m int) ([]int64,[]int64,[]int64) { a,b,c := ia64(m),ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi64(),gi64(),gi64()}; return a,b,c }
+func fill464(m int) ([]int64,[]int64,[]int64,[]int64) { a,b,c,d := ia64(m),ia64(m),ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi64(),gi64(),gi64(),gi64()}; return a,b,c,d }
+func abs64(a int64) int64 { if a < 0 { return -a }; return a }
+func rev64(a []int64) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max64(a,b int64) int64 { if a > b { return a }; return b }
+func min64(a,b int64) int64 { if a > b { return b }; return a }
+func maxarr64(a []int64) int64 { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr64(a []int64) int64 { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr64(a []int64) int64 { ans := int64(0); for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr64(a []int64) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod64(a,e,mod int64) int64 { res, m := int64(1), a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint64(a,e int64) int64 { res, m := int64(1), a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd64(a,b int64) int64 { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended64(a,b int64) (int64,int64,int64) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended64(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv64(a,m int64) (int64,bool) { g,x,_ := gcdExtended64(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecint64string(a []int64) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.FormatInt(a,10) }; return strings.Join(astr," ") }
+func makefact64(n int,mod int64) ([]int64,[]int64) {
+	fact,factinv := make([]int64,n+1),make([]int64,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * int64(i) % mod }
+	factinv[n] = powmod64(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * int64(i+1) % mod }
+	return fact,factinv
 }
 func main() {
 	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
 	defer wrtr.Flush()
-	infn := ""
-	if infn == "" && len(os.Args) > 1 {
-		infn = os.Args[1]
-	}
-	if infn != "" {
-		f, e := os.Open(infn)
-		if e != nil {
-			panic(e)
-		}
-		rdr = bufio.NewScanner(f)
-	}
-	rdr.Split(bufio.ScanWords)
-	rdr.Buffer(make([]byte, 1024), 1_000_000_000)
+	infn := ""; if infn == "" && len(os.Args) > 1 {	infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1_000_000_000)
 	// PROGRAM STARTS HERE
-	T := gi()
-	for tt := 1; tt <= T; tt++ {
-		N, M := gi(), gi()
-		gr := make([][]int, N)
-		for i := 0; i < N; i++ {
-			gr[i] = gis(M)
-		}
+	T := gi();
+	for tt:=1;tt<=T;tt++ {
+		N,M := gi(),gi(); gr := make([][]int,N); for i:=0;i<N;i++ { gr[i] = gis(M); }
 		// Just need to check if zero is in between the max and min and has the right parity
-		dpmax := twodi(N, M, -999999999)
-		dpmin := twodi(N, M, 999999999)
+		dpmax := twodi(N,M,-999999999)
+		dpmin := twodi(N,M,999999999)
 		dpmax[0][0] = gr[0][0]
 		dpmin[0][0] = gr[0][0]
-		for i := 0; i < N; i++ {
-			for j := 0; j < M; j++ {
-				if i > 0 {
-					dpmax[i][j] = max(dpmax[i][j], dpmax[i-1][j]+gr[i][j])
-					dpmin[i][j] = min(dpmin[i][j], dpmin[i-1][j]+gr[i][j])
-				}
-				if j > 0 {
-					dpmax[i][j] = max(dpmax[i][j], dpmax[i][j-1]+gr[i][j])
-					dpmin[i][j] = min(dpmin[i][j], dpmin[i][j-1]+gr[i][j])
-				}
+		for i:=0;i<N;i++ {
+			for j:=0;j<M;j++ {
+				if i > 0 { dpmax[i][j] = max(dpmax[i][j],dpmax[i-1][j]+gr[i][j]); dpmin[i][j] = min(dpmin[i][j],dpmin[i-1][j]+gr[i][j]) }
+				if j > 0 { dpmax[i][j] = max(dpmax[i][j],dpmax[i][j-1]+gr[i][j]); dpmin[i][j] = min(dpmin[i][j],dpmin[i][j-1]+gr[i][j]) }
 			}
 		}
 		ans := "NO"
-		if (N+M)%2 == 1 && dpmax[N-1][M-1] >= 0 && dpmin[N-1][M-1] <= 0 {
-			ans = "YES"
-		}
-		fmt.Fprintln(wrtr, ans)
+		if (N+M) % 2 == 1 && dpmax[N-1][M-1] >= 0 && dpmin[N-1][M-1] <= 0 { ans = "YES" }
+		fmt.Fprintln(wrtr,ans)
 	}
 }
