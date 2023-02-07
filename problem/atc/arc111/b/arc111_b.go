89f8cd64e7fef73f9de888a7b293960959890af8
liushuliang <shuliang.liu@capstones.cn>
2023-02-07 14:30:49 +0800

    arc111_b.go 转化为树统计

--- problem/atc/arc111/b/arc111_b.go
+++ problem/atc/arc111/b/arc111_b.go
@@ -11,7 +11,7 @@ func run(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	var n, ans, u, v, cnt_u, cnt_e int
+	var n, ans, u, v, cntU, cntE int
 	Fscan(in, &n)
 	g := make([][]int, 4e5+1)
 	for i := 0; i < n; i++ {
@@ -23,8 +23,8 @@ func run(_r io.Reader, _w io.Writer) {
 	var dfs func(int)
 	dfs = func(u int) {
 		vis[u] = true
-		cnt_u += 1
-		cnt_e += len(g[u])
+		cntU += 1
+		cntE += len(g[u])
 		for _, v := range g[u] {
 			if !vis[v] {
 				dfs(v)
@@ -36,9 +36,9 @@ func run(_r io.Reader, _w io.Writer) {
 	// 并查集方法用py实现；dfs方法用go实现；bfs方法用rust实现；可移步查看
 	for u, b := range vis {
 		if !b && g[u] != nil {
-			cnt_u, cnt_e = 0, 0
+			cntU, cntE = 0, 0
 			dfs(u)
-			ans += min(cnt_u, cnt_e/2) // 树则边数，有环则节点数，因此边不用统计完，因此可以用vis
+			ans += min(cntU, cntE/2) // 树则边数，有环则节点数，因此边不用统计完，因此可以用vis
 		}
 	}
 	Fprintln(out, ans)
