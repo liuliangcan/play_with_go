8a5a261b8a1c3907875d5041ca5ae25034553141
liushuliang <shuliang.liu@capstones.cn>
2023-04-03 17:02:50 +0800

    cf1469c.go 模拟上下界

---
+++ problem/cf/cf1400_1499/cf1469/c/cf1469c_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	t.Log("Current test is [c]")
+	testCases := [][2]string{
+		{`3
+6 3
+0 0 2 5 1 1
+2 3
+0 2
+3 2
+3 0 2`, `YES
+YES
+NO`},
+	}
+	testutil.AssertEqualStringCase(t, testCases, 0, run)
+}
+
+// 本模板由https://github.com/liuliangcan/play_with_python/blob/main/tools/gen_code_tool/gen_template.py自动生成,在本目录下使用go test进行case测试
+// https://codeforces.com/problemset/problem/1469/C
+// https://codeforces.com/problemset/problem/1469/C/submit?taskScreenName=cf1469c
