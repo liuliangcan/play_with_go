12db144410ce6a12b70580de185153570916306c
liushuliang <shuliang.liu@capstones.cn>
2023-04-04 14:20:36 +0800

    cf1213d2.go 贪心

---
+++ problem/cf/cf1200_1299/cf1213/d2/cf1213d2_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	t.Log("Current test is [d2]")
+	testCases := [][2]string{
+		{`5 3
+1 2 2 4 5`, `1`},
+		{`5 3
+1 2 3 4 5`, `2`},
+		{`5 3
+1 2 3 3 3`, `0`},
+	}
+	testutil.AssertEqualStringCase(t, testCases, 0, run)
+}
+
+// 本模板由https://github.com/liuliangcan/play_with_python/blob/main/tools/gen_code_tool/gen_template.py自动生成,在本目录下使用go test进行case测试
+// https://codeforces.com/problemset/problem/1213/D2
+// https://codeforces.com/problemset/problem/1213/D2/submit?taskScreenName=cf1213d2
