b8c6d6b504db90d1335e3210b07c7bc377f99bd8
liushuliang <shuliang.liu@capstones.cn>
2023-06-01 11:55:42 +0800

    cf190d.go 同向双指针

---
+++ problem/cf/cf100_199/cf190/d/cf190d.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+func solve(in io.Reader, out io.Writer) {
+	var n, k, ans int
+	Fscan(in, &n, &k)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		Fscan(in, &a[i])
+	}
+	cnt := map[int]int{}
+	l := 0
+	for _, v := range a {
+		cnt[v]++
+		for cnt[v] >= k {
+			cnt[a[l]]--
+			l++
+		}
+		ans += l
+	}
+	Fprintln(out, ans)
+}
+func run(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	solve(in, out)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
